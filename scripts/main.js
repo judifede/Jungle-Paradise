0d01e7540e55c867dbdde3741a0378caddd25bc4
Diego Fernandez <judifede217@gmail.com>
2024-02-07 15:17:34 +0000

    function countdown and play reordered

--- scripts/main.js
+++ scripts/main.js
@@ -15,57 +15,53 @@ soundWallpaper.volume = 0.8
 soundWallpaper.currentTime = 14
 
 
-
 function start() {
     canvas.classList.add('opened')
     containerStartup.classList.add('hidden')
+}
 
-    
-}   
-
-function play() {
-    
+function countdown() {
     countdownGame.classList.add("opened")
     let i = 0
-    const colors = ["red","orange","yellow","green"]
+    const colors = ["red", "orange", "yellow", "green"]
     countdownGameCounter.style.color = `${colors[i]}`
-    
-    const countdown = setInterval(()=>{
+
+    const countdown = setInterval(() => {
         countdownGameCounter.innerText = --countdownGameCounter.innerText
         i++
         countdownGameCounter.style.color = `${colors[i]}`
-        if(countdownGameCounter.innerText === '0') countdownGameCounter.innerText = 'GO!'
-    },1000)
+        if (countdownGameCounter.innerText === '0') countdownGameCounter.innerText = 'GO!'
+    }, 1000)
 
-    setTimeout(()=>{
-        let currentTimeGame = parseInt(timeGame.innerText.slice(0, 2))
-        
-        const timeGameId = setInterval(() => {
-            timeGame.innerText = --currentTimeGame + ' s'
-            soundWallpaper.play()
-            
-            if (currentTimeGame <= 5) {
-                timeGame.classList.add('endingTime')
-            }
-            
-            if (currentTimeGame <= 0){
-                clearInterval(timeGameId)
-                clearInterval(objectiveId)
-                endGame()
-            }
-        }, 1000)
-
-        playGame.classList.add("hidden") //Hidden our play button
-        createObjective()
-
+    playGame.classList.add("hidden") //Hidden our play button
+    setTimeout(() => {
         clearInterval(countdown)
         countdownGameCounter.innerText = '3'
         i = 0
         countdownGame.classList.remove('opened')
-    },4000)
+        play()
+    }, 4000)
+}
 
-    
+function play() {
+    let currentTimeGame = parseInt(timeGame.innerText.slice(0, 2))
 
+    const timeGameId = setInterval(() => {
+        timeGame.innerText = --currentTimeGame + ' s'
+        soundWallpaper.play()
+
+        if (currentTimeGame <= 5) {
+            timeGame.classList.add('endingTime')
+        }
+
+        if (currentTimeGame <= 0) {
+            clearInterval(timeGameId)
+            clearInterval(objectiveId)
+            endGame()
+        }
+    }, 1000)
+
+    createObjective()
 }
 
 function createObjective() {
@@ -85,7 +81,7 @@ function removeObjective(objective) {
     objective.removeObjective()
 }
 
-function endGame(){
+function endGame() {
     //Hidden all objectives from last game
     for (let i = 0; i < document.getElementsByClassName("objective").length; i++) {
         const element = document.getElementsByClassName("objective")[i];
@@ -109,7 +105,7 @@ function resetGame() {
 
 }
 
-playGame.addEventListener("click", play)
+playGame.addEventListener("click", countdown)
 startGame.addEventListener("click", start)
 
 
