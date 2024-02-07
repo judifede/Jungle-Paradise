53f1765d40904a1d19ae6316aafce6db9dcef83b
Aitor Díaz Santana <aitordd.ss@gmail.com>
2024-02-07 11:17:06 +0000

    emerginPoint done

--- scripts/objective.js
+++ scripts/objective.js
@@ -1,10 +1,11 @@
-
 class Objective {
     constructor(x, y, parent) {
         this.x = x
         this.y = y
         this.parent = parent
         this.sprite
+        this.score = 500
+        this.point 
     }
 
     createObjective() {
@@ -13,8 +14,9 @@ class Objective {
         objective.style.top = this.y + "%"
         objective.style.left = this.x + "%"
 
-        objective.addEventListener("click", () => {   
+        objective.addEventListener("click", (e) => {   
             this.objectiveHitted()
+            this.emergingPoint(e)
         })
         this.sprite = objective
         this.parent.appendChild(objective)
@@ -24,11 +26,34 @@ class Objective {
         if(this.parent.contains(this.sprite)) this.parent.removeChild(this.sprite)
     }
 
-    objectiveHitted() {
-        this.parent.removeChild(this.sprite)
-        let totalScoreNode = document.getElementById("totalScore")
-        let currentScore =  parseInt(totalScoreNode.innerText);
-        currentScore += 500
-        totalScoreNode.innerText = currentScore
-    }
+
+objectiveHitted() {
+    console.log(this.parent)
+    this.parent.removeChild(this.sprite)
+    let totalScoreNode = document.getElementById("totalScore")
+    let currentScore =  parseInt(totalScoreNode.innerText);
+    currentScore += this.score
+    totalScoreNode.innerText = currentScore
+}
+
+
+emergingPoint(event){
+    let x = event.clientX
+    let y = event.clientY
+    this.point = document.createElement('p')
+    this.point.innerText = '+' + this.score
+    this.point.style.position = 'absolute'
+    this.point.style.left = 45 + x + 'px'
+    this.point.style.top = y-65 + 'px'
+    this.point.classList.add('emergingPoint')
+    document.querySelector('body').appendChild(this.point)
+    setTimeout(()=>{
+        document.querySelector('body').removeChild(this.point)
+
+    },500)
+    
+
+}
+
+
 }
